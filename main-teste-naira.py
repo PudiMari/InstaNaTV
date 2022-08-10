fa86469d5cd1a6965734791efc83a344545a0160
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-08-10 12:26:07 -0400

    Update main-teste-naira.py

--- main-teste-naira.py
+++ main-teste-naira.py
@@ -1,18 +1,18 @@
-from  pil_video  import  make_video 
+from  naira  import  make_video 
 """ 
 Faça uma lista de imagens PIL na sequência em que deseja fazer um vídeo, digamos que 
 você tenha feito uma lista chamada test 
 """ 
 
-teste = ['C:/Users/Naíra Souza/Desktop/Naíra/Sistemas Embarcados/InstaNaTV-master/teste/2020-04-03_01-39-12_UTC_1.jpg', 
-         'C:/Users/Naíra Souza/Desktop/Naíra/Sistemas Embarcados/InstaNaTV-master/teste/2020-04-03_01-46-37_UTC.jpg'
+img = ['./teste/2020-04-03_01-39-12_UTC_1.jpg', 
+         './teste/2020-04-03_01-46-37_UTC.jpg'
          ]
 # O primeiro argumento é a lista composta por imagens PIL 
 # o segundo argumento é o FPS do vídeo que você deseja gerar 
 # se houver mais imagens e você especificar menos FPS, o vídeo será mais longo 
 # A API abaixo gerará o vídeo, reproduza-o. No processo, ele usará seu diretório /tmp 
 # para criar alguns arquivos temporários e eles serão excluídos após a reprodução do vídeo. 
-make_video (teste,  fps = 0.5, delete_folder = False)
+make_video (img,  fps = 0.5, play_video = False)
 # há um terceiro argumento opcional delete_folder, que é o padrão True, mas se você quiser ter o vídeo e 
 # imagens para sua referência, você pode passar isso como false, para que não seja excluído automaticamente 
 #make_video (teste ,  fps = 20 ,  delete_folder = False ) 
