fe96ee9736f743372a7c30796475936cb0dc4d50
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-08-29 19:51:09 -0400

    Update teste2-radio.py

--- teste2-radio.py
+++ teste2-radio.py
@@ -3,32 +3,15 @@ import sys
 import subprocess
 import shlex
 
-CaminhoArquivoControle="/tmp/ControleRadio"
-
 def TocaPlaylist():
     
-    ComandoMPlayer = "mplayer -input file=/tmp/ControleRadio -slave -playlist "
+    ComandoMPlayer = "mplayer -input file=/tmp/ControleRadio.txt -slave -playlist "
     Playlists=["https://player.hdradios.net/player-home-page/6990"]        
     NomesRadios=["Radio IFMT Campus Cuiaba"] 
  
     os.system("pkill -f mplayer")
     ComandoPlaylist = ComandoMPlayer + Playlists[0]
-     
-    FNULL = open(os.devnull,'w')
-    args = shlex.split(ComandoPlaylist)
-    InterfaceMPlayer = subprocess.Popen(args, shell=False, stdin=subprocess.PIPE, stdout=FNULL, stderr=subprocess.STDOUT)
+    
     return
-
-def CriaArquivoControle():
-    if (os.path.exists(CaminhoArquivoControle)):
-        return
- 
-    try:
-        os.mkfifo(CaminhoArquivoControle)
-    except:
-        print ("Falha ao criar arquivo de controle. Por favor, verifique o caminho de criacao do arquivo")
-        exit(1)
-
-CriaArquivoControle()
 
 TocaPlaylist()
