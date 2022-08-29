cdc5faff276584691550cd5e6180a19c7f09bc58
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-08-29 12:51:09 -0400

    Update clip-imagens.py

--- clip-imagens.py
+++ clip-imagens.py
@@ -1,25 +1,43 @@
+import asyncio
+from asyncore import loop
+from operator import concat
 from os import walk
 from moviepy.editor import *
 
 files = []
-files_list = []
+images_list = []
+videos_list = []
 path = './ifmtcuiabaoficial'
+video = 'clips.mp4'
 
 for (dirpath, dirnames, filenames) in walk(path):
     files.extend(filenames)
     break
 
-tamanho = len(files)
+tamanho_files = len(files)
 
-for i in range(tamanho):
+for i in range(tamanho_files):
     if files[i].endswith('.jpg'):
-        files_list.append(os.path.join(path, files[i]))
-    
+        images_list.append(os.path.join(path, files[i]))
+    elif files[i].endswith('.mp4'):
+        videos_list.append(os.path.join(path, files[i]))
+        
 fps = 24
-clips = [ImageClip(m).set_duration(4) for m in files_list]
 
-print(clips)
+clips_image = [ImageClip(m).set_duration(4) for m in images_list]
     
-concat_clip = concatenate_videoclips(clips, method='compose')
+concat_clip_image = concatenate_videoclips(clips_image, method = 'compose')
 
-concat_clip.write_videofile('clips.mp4', fps=fps)
+tamanho_videos_list = len(videos_list)
+
+clips_video = [VideoFileClip(videos_list[c]) for c in range(tamanho_videos_list)]
+
+concat_clip_video = concatenate_videoclips(clips_video, method = 'compose')
+
+concat_all = concatenate_videoclips([concat_clip_video, concat_clip_image], method = 'compose')
+
+concat_all.write_videofile(video, fps=fps)
+
+
+
+
