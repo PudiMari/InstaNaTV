ef996acf4c90eddab433b90290d6140aff507a04
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-08-28 23:49:46 -0400

    Update clip-imagens.py

--- clip-imagens.py
+++ clip-imagens.py
@@ -12,7 +12,8 @@ for (dirpath, dirnames, filenames) in walk(path):
 tamanho = len(files)
 
 for i in range(tamanho):
-    files_list.append(os.path.join(path, files[i]))
+    if files[i].endswith('.jpg'):
+        files_list.append(os.path.join(path, files[i]))
     
 fps = 24
 clips = [ImageClip(m).set_duration(4) for m in files_list]
